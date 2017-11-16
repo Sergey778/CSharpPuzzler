51aca7e830814b71f887381634bf073a6da7bf58
Sergey778 <sergeygerasimov778@gmail.com>
2017-11-16 16:28:11 +0300

    Add new puzzler
    New puzzler (PuzzlerB8 project) is about using method groups

---
+++ PuzzlerB8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PuzzlerB8
+{
+    internal static class Program
+    {
+
+        private static int Foo(int a) => a + 1;
+
+        private static Func<TIn, TOut> AsLambda<TIn, TOut>(Func<TIn, TOut> func) => func;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AsLambda(Foo)(1));
+        }
+    }
+}
