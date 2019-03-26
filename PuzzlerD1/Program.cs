c30c2b27d9639ba50c037f15fb08855cc11a2b15
ivanov.as <ivanov.as@medindex.ru>
2019-03-26 14:27:42 +0300

    New one

---
+++ PuzzlerD1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PuzzlerD1
+{
+    public struct Test
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test { Name = "Hello" };
+            var list = new List<Test>();
+            list.Add(test);
+            test.Name = "World";
+            Console.WriteLine(list.First().Name);
+        }
+    }
+}
